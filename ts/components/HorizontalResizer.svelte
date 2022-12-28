eedb9ef3cfaf5327207f3392396717e70fbba9a1
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2022-12-28 13:39:21 -0700

    Allow HorizontalResizer to push other resizers.
    Adds option `pushOtherPanes` to HorizontalResizer component in order to
    allow resizer pane pushing when more than two panes are in use. This is
    similar to the Svelte-Splitpanes or vue-splitpanes libraries.
    
    This could also be added to VerticalResizer, but I haven't done it yet.

--- ts/components/HorizontalResizer.svelte
+++ ts/components/HorizontalResizer.svelte
@@ -15,6 +15,8 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
     export let panes: ResizablePane[];
     export let index = 0;
+    export let after_index = index + 1;
+    export let pushOtherPanes: boolean = false;
     export let tip = "";
     export let showIndicator = false;
     export let clientHeight: number;
@@ -41,7 +43,7 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
         }
     }
 
-    function onMove(this: Window, { movementY }: PointerEvent): void {
+    function onMove_simple(movementY: number): void {
         if (movementY < 0) {
             if (after.height - movementY <= after.maxHeight) {
                 const resized = before.resizable.getHeightResizer().resize(movementY);
@@ -60,6 +62,81 @@ License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
                 .getHeightResizer()
                 .resize(before.height - before.maxHeight);
             before.resizable.getHeightResizer().resize(-resized);
+        }
+    }
+
+    function onMove_pushOtherPanes(movementY: number): void {
+        let i = index;
+        let j = after_index;
+        while (true) {
+            let dy = 0;
+            /* Look for the first "before" pane that can be resized. */
+            for (; i >= 0; i--) {
+                before = panes[i];
+                if (movementY < 0) {
+                    // Mouse moving up.
+                    if (before.height > before.minHeight) {
+                        dy = Math.max(movementY, before.minHeight - before.height);
+                        break; // Resizable pane found; break out of for loop.
+                    }
+                } else {
+                    // Mouse moving down.
+                    if (before.height < before.maxHeight) {
+                        dy = Math.min(movementY, before.maxHeight - before.height);
+                        break; // Resizable pane found; break out of for loop.
+                    }
+                }
+            }
+            /* Look for the first "after" pane that can be resized. */
+            for (; j < panes.length; j++) {
+                after = panes[j];
+                if (movementY < 0) {
+                    // Mouse moving up.
+                    if (after.height < after.maxHeight) {
+                        dy = Math.max(dy, after.height - after.maxHeight);
+                        break; // Resizable pane found; break out of for loop.
+                    }
+                } else {
+                    // Mouse moving down.
+                    if (after.height > after.minHeight) {
+                        dy = Math.min(dy, after.height - after.minHeight);
+                        break; // Resizable pane found; break out of for loop.
+                    }
+                }
+            }
+            /* If i < 0 here then no "before" panes can be resized. Similarly if
+             * j >= panes.length then no "after" panes can be resized.
+             */
+            if ((i < 0) || (j >= panes.length) || (dy == 0)) { break; }
+            /* If the "before" pane isn't resized first during fast upward mouse
+             * motion, or the "after" pane isn't resized first during fast
+             * downward motion, there's some glitching in the web view.
+             */
+            let resized = 0;
+            if (movementY < 0) {
+                // Mouse moving up.
+                resized = before.resizable.getHeightResizer().resize(dy);
+                after.resizable.getHeightResizer().resize(-resized);
+                movementY -= resized;
+            } else {
+                // Mouse moving down.
+                resized = after.resizable.getHeightResizer().resize(-dy);
+                before.resizable.getHeightResizer().resize(-resized);
+                movementY += resized;
+            }
+            /* On each iteration of the while loop, we (hopefully) make progress
+             * towoard reszing by movementY. We step when either the goal is
+             * reached or we can't make any more progress.
+             */
+            if ((movementY == 0) || (resized == 0)) { break; }
+        }
+    }
+
+    function onMove(this: Window, { movementY }: PointerEvent): void {
+        if (pushOtherPanes) {
+            onMove_pushOtherPanes(movementY);
+        } else {
+            onMove_simple(movementY);
         }
     }
 
