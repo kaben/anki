417e7371490c9f60568c199fcdc26e09717f0ed2
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2023-01-21 23:57:52 -0700

    Add version info parsing.
    This adds a `VersionInfo` struct for info parsed from Anki client
    version strings, together with functions `parse_version_info()` and
    `parse_short_version_info()` to do the parsing. These handle optional
    extra `variant` info appended to the buildhash. The purpose is for the
    Anki sync server to detect AnkiMath clients.
    
    `anki,2.1.57+alpha+fubar (6ad33e70.ankimath),lin:ubuntu:22.04`, for
    example, would parse to:
    
    - name: anki
    - major: 2
    - minor: 1
    - patch: 57
    - prerelease: alpha
    - buildmetadata: fubar
    - buildhash: 6ad33e70
    - variant: ankimath
    - platform: lin:ubuntu:22.04

--- rslib/src/version.rs
+++ rslib/src/version.rs
@@ -1,9 +1,205 @@
 // Copyright: Ankitects Pty Ltd and contributors
 // License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+use regex::Regex;
 use std::env;
 
 use lazy_static::lazy_static;
+
+use crate::prelude::*;
+
+pub const ANKIMATH_VARIANT: &str = "mxyzptlk";
+
+#[derive(Debug, Default)]
+pub struct VersionInfo {
+    pub name: String,
+    pub major: u32,
+    pub minor: u32,
+    pub patch: u32,
+    pub prerelease: String,
+    pub buildmetadata: String,
+    pub buildhash: String,
+    pub variant: String,
+    pub platform: String,
+}
+
+pub fn client_version_re() -> &'static Regex {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(
+            r"(?x)
+            ^
+            (?:(?P<name>\S+),)?
+            (?P<major>0|[1-9]\d*)
+            \.
+            (?P<minor>0|[1-9]\d*)
+            \.
+            (?P<patch>0|[1-9]\d*)
+            (?:-(?P<prerelease>
+                (?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)
+                (?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*
+              )
+            )?(?:\+(?P<buildmetadata>
+                [0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*
+              )
+            )?
+            \s+
+            \(
+            (?P<buildhash>[0-9a-zA-Z-]+)
+            \.?
+            (?P<variant>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*)?
+            \)
+            ,
+            (?P<platform>\S+)
+            $
+            "
+        )
+        .unwrap();
+    }
+    &RE
+}
+
+pub fn short_client_version_re() -> &'static Regex {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(
+            r"(?x)
+            ^
+            (?P<majorver>0|[1-9]\d*)
+            \.
+            (?P<minorver>0|[1-9]\d*)
+            \.
+            (?P<patchver>0|[1-9]\d*)
+            (?:-(?P<prerelease>
+                (?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)
+                (?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*
+              )
+            )?(?:\+(?P<buildmetadata>
+                [0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*
+              )
+            )?
+            ,
+            (?P<buildhash>[0-9a-zA-Z-]+)
+            \.?
+            (?P<variant>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*)?
+            ,
+            (?P<platform>\S+)
+            $
+            "
+        )
+        .unwrap();
+    }
+    &RE
+}
+
+pub fn parse_version_info(version: &str) -> Result<Option<VersionInfo>> {
+    println!("parse_version_info(): version: {:?}", version);
+    //let caps = client_version_re().captures(version).unwrap();
+    match client_version_re().captures(version) {
+        Some(caps) => {
+            let name: String = match caps.name("name") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            let major: u32 = match caps.name("major") {
+                Some(text) => text.as_str().parse().unwrap(),
+                _ => 0,
+            };
+            let minor: u32 = match caps.name("minor") {
+                Some(text) => text.as_str().parse().unwrap(),
+                _ => 0,
+            };
+            let patch: u32 = match caps.name("patch") {
+                Some(text) => text.as_str().parse().unwrap(),
+                _ => 0,
+            };
+            let prerelease: String = match caps.name("prerelease") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            let buildmetadata: String = match caps.name("buildmetadata") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            let buildhash: String = match caps.name("buildhash") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            let variant: String = match caps.name("variant") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            let platform: String = match caps.name("platform") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            Ok(Some(VersionInfo {
+                name,
+                major,
+                minor,
+                patch,
+                prerelease,
+                buildmetadata,
+                buildhash,
+                variant,
+                platform,
+            }))
+        }
+        None => Ok(None),
+    }
+}
+
+pub fn parse_short_version_info(version: &str) -> Result<Option<VersionInfo>> {
+    println!("parse_short_version_info(): version: {:?}", version);
+    //let caps = short_client_version_re().captures(version).unwrap();
+    match short_client_version_re().captures(version) {
+        Some(caps) => {
+            let name: String = "".to_string();
+            let major: u32 = match caps.name("major") {
+                Some(text) => text.as_str().parse().unwrap(),
+                _ => 0,
+            };
+            let minor: u32 = match caps.name("minor") {
+                Some(text) => text.as_str().parse().unwrap(),
+                _ => 0,
+            };
+            let patch: u32 = match caps.name("patch") {
+                Some(text) => text.as_str().parse().unwrap(),
+                _ => 0,
+            };
+            let prerelease: String = match caps.name("prerelease") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            let buildmetadata: String = match caps.name("buildmetadata") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            let buildhash: String = match caps.name("buildhash") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            let variant: String = match caps.name("variant") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            let platform: String = match caps.name("platform") {
+                Some(text) => text.as_str().to_string(),
+                _ => "".to_string(),
+            };
+            Ok(Some(VersionInfo {
+                name,
+                major,
+                minor,
+                patch,
+                prerelease,
+                buildmetadata,
+                buildhash,
+                variant,
+                platform,
+            }))
+        }
+        None => Ok(None),
+    }
+}
 
 pub fn version() -> &'static str {
     include_str!("../../.version").trim()
@@ -16,9 +212,10 @@ pub fn buildhash() -> &'static str {
 pub(crate) fn sync_client_version() -> &'static str {
     lazy_static! {
         static ref VER: String = format!(
-            "anki,{version} ({buildhash}),{platform}",
+            "anki,{version} ({buildhash}.{variant}),{platform}",
             version = version(),
             buildhash = buildhash(),
+            variant = ANKIMATH_VARIANT,
             platform = env::var("PLATFORM").unwrap_or_else(|_| env::consts::OS.to_string())
         );
     }
@@ -28,7 +225,7 @@ pub(crate) fn sync_client_version() -> &'static str {
 pub(crate) fn sync_client_version_short() -> &'static str {
     lazy_static! {
         static ref VER: String = format!(
-            "{version},{buildhash},{platform}",
+            "{version},{buildhash}.mxyzptlk,{platform}",
             version = version(),
             buildhash = buildhash(),
             platform = env::consts::OS
